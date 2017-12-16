b994b681820d49ca592200581e27d6a33ef5d76d
pacpac1992 <juankarlos.0304@gmail.com>
2017-12-15 20:27:05 -0600

    Se crearon funciones para 404, login, cookies, etc

--- forms.py
+++ forms.py
@@ -1,7 +1,12 @@
 from wtforms import Form
-from wtforms import StringField, TextField
+from wtforms import StringField, TextField, PasswordField
 from wtforms.fields.html5 import EmailField
 from wtforms import validators
+from wtforms import HiddenField
+
+def length_honeypot(form,field):
+	if len(field.data) > 0:
+		raise validators.ValidationError("El campo debe estar vacio")
 
 class CommentForm(Form):
 	username = StringField('username',
@@ -17,5 +22,20 @@ class CommentForm(Form):
 
 	comment = TextField('Comentario',
 		[
+			validators.required(message="Requiere usuario"),
+		])
+	honeypot = HiddenField("",[length_honeypot])
+
+class FormLogin(Form):
+	username = StringField('username',
+		[
+			validators.length(min=4,max=35,message="Ingrese usuario valido"),
 			validators.required(message="Requiere usuario")
 		])
+	password = PasswordField('password',
+		[
+			validators.length(min=4,max=35,message="Ingrese passwrod valido"),
+			validators.required(message="Requiere password")
+		])
+	honeypot = HiddenField("",[length_honeypot])
+		
--- main.py
+++ main.py
@@ -2,22 +2,77 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+from flask import flash
+
+from flask import redirect
+from flask import url_for
+
+from flask import session
+
+from flask import make_response
+
+# proteccion
+from flask_wtf import CSRFProtect
 
 import forms
 
 app = Flask(__name__)
 # app = Flask(__name__,template_folder ='prueba')
 
-@app.route('/',methods=['GET','POST'])
+app.secret_key = "my_secret_key"
+csrf = CSRFProtect(app)
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html')
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+	loginform = forms.FormLogin(request.form)
+	if request.method == 'POST' and loginform.validate():
+		session['username'] = loginform.username.data
+		username_1 = loginform.username.data
+		success_messages = "Bienvenido {}".format(username_1)
+		flash(success_messages)
+
+	title = "Curso flask login"
+	return render_template('login.html',title=title,form=loginform)
+
+@app.route('/logout')
+def logout():
+	if 'username' in session:
+		session.pop('username')
+	return redirect(url_for('index'))
+
+@app.route('/cookie')
+def cookie():
+	response = make_response(render_template('cookie.html'))
+	response.set_cookie('custom_cookie','jose')
+	return response
+
+@app.route('/')
 def index():
+	custom_cookie = request.cookies.get('custom_cookie')
+	print custom_cookie
+
+	if 'username' in session:
+		username = session['username']
+		print username
+	title = "Curso flask"
+	return render_template('index.html',title=title)
+	
+@app.route('/comment',methods=['GET','POST'])
+def comment():
 	comment_form = forms.CommentForm(request.form)
 	if request.method == 'POST' and comment_form.validate():
 		print comment_form.username.data
 		print comment_form.email.data
 		print comment_form.comment.data
+	else:
+		print "Error en formulario"
 
-	title = "Curso flask"
-	return render_template('index.html',title=title,form=comment_form)
+	title = "comments"
+	return render_template('comment.html',title=title,form=comment_form)
 
 if __name__ == '__main__':
 	app.run(debug = True,port = 8000)
